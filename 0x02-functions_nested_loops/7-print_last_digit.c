a509864d77cbab41bd31f79a9f19aca95bef5e5d
imbukwa1 <imbukwaaudry@gmail.com>
2023-07-19 02:40:29 -0700

    back

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,14 @@
 
 /**
  * print_last_digit - prints the last digit of the a number
- * @n the int to extract the last digit from
- * Return value of the last digit
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
 int print_last_digit(int n)
 {
 	int a;
 
-	if(n < 0)
+	if (n < 0)
 	n = -n;
 
 	a = n % 10;
